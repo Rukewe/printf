7629fdf798d66f2daed0ef448193ee0985735ce4
Rukewe <rukewe.obominuru@gmail.com>
2022-04-19 10:38:45 -0700

    task 1

--- specifer.c
+++ specifer.c
@@ -1,34 +1,84 @@
 #include "main.h"
-/**
- * use_c - when format is c
- * @arg: argument
- * Return: 1 if there's character, -1 if not
- */
 
-int use_c(va_list arg)
+/**
+ * print_int - prints an integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_int(va_list l, flags_t *f)
 {
-	return (_putchar(va_arg(arg, int)));
+	int n = va_arg(l, int);
+	int res = count_digit(n);
+
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
 }
+
 /**
- * use_s - when format is s
- * @arg: argument
- * Return: length
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-
-int use_s(va_list arg)
+int print_unsigned(va_list l, flags_t *f)
 {
-	/*declare a char pointer*/
-	char *s;
-	int i;
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
-	s = va_arg(arg, char *);
+	(void)f;
+	return (_puts(str));
+}
 
-	if (s == NULL)/*if string is NULL*/
-		s = "(null)";
-	else if (*s == '\0')/*checking for empty string*/
-		return (-1);
-	for (i = 0; s[i]; i++)
-		_putchar(s[i]);
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
 
-	return (i);
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
+
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
 }
