0709961d4cd743998b8c7f107784ff901e65f682
Rukewe <rukewe.obominuru@gmail.com>
2022-04-19 10:05:28 -0700

    Updated

--- _printf.c
+++ _printf.c
@@ -1,51 +1,49 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to output
- * @format: argument
- * Return: res
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
  */
-
 int _printf(const char *format, ...)
 {
-	/*declare variable and initialize struct to be used*/
-	int i, sum = 0, count = 0;
-	va_list arg;
-	/* initialize arg with format*/
-	va_start(arg, format);
-	if (format == NULL)
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{/*it is a normal character so print*/
-			_putchar(format[i]);
-			sum++;
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{/*means it's %% so it should print %*/
-			_putchar('%');
-			sum++;
-			i++;/*forgot to do this*/
-			continue;
-		}
-		while (format[i + 1] == ' ')
-			i++;
-		if (format[i + 1] == '\0')
-			return (-1);
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-		count = count_spec(format[i + 1], arg);/*the count of arg*/
-		if (count == -1 || count != 0)
-			i++;
-		if (count > 0)/*count_spec used and the specifier is correct*/
-			sum += count;
-		if (count == 0)/*invalid specifier, treat as normal character*/
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
 		{
-			_putchar('%');
-			sum++;
+			sum += _putchar(*p);
+			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
 	}
-	va_end(arg);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
 	return (sum);
 }
